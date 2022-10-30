d309c7b3130637c1bc430155ffbe852abab3862b
pcidsoto <pitercid@hotmail.com>
2022-10-30 15:00:12 -0300

    ejercicios clases

---
+++ Ejercicio 1/tipos.js
@@ -0,0 +1,16 @@
+// tipos primitivos
+
+// numbers
+4;
+
+// strings
+"Hola mundo";
+'Hola mundo';
+
+// bool
+true;
+false;
+
+// special nulo e indefinido
+null;
+undefined;
---
+++ Ejercicio 1/variables.js
@@ -0,0 +1,9 @@
+const constante = "hola soy una constante";
+console.log(constante);
+
+var a = 1; // Su valor puede cambiar
+console.log(a);
+a = 5
+console.log(a);
+
+let b = 10;
