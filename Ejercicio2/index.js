f99f52d7ced3e33660fc9c1baacbb49c0e1b8c08
pcidsoto <pitercid@hotmail.com>
2022-10-30 14:58:38 -0300

    ejercicio 2

---
+++ Ejercicio2/index.js
@@ -0,0 +1,11 @@
+
+const birth_date = new Date(1991, 9,12);
+const favorite_book = {
+    title:"La casa de los esppíritus",
+    author: "isabel allende",
+    date: new Date(1982, 0, 1),
+    url:"https://www.casadellibro.com/libro-la-casa-de-los-espiritus/9788483462034/1105138"
+};
+const lista = ["Pedro", 31, true, birth_date, favorite_book];
+
+console.log(lista);
